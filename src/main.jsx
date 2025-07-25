5c0354853eb745445d03558515099638aabb62e3
mtm <Stafr95@mail.ru>
2025-07-25 21:13:23 +0500

    using redux with native js

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,65 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
-import App from './App.jsx'
+import {createStore} from "redux";
 
-import './index.css'
 
-const initialState = 0;
+const initialState = {value: 0};
 
-const reducer = (state, action) => {
+const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "INC":
-      return state + 1;
+      return {
+        ...state,
+        value: state.value + 1
+      }
+    case "DEC":
+      return {
+        ...state,
+        value: state.value -1
+      }
+    case "RND":
+      return {
+        ...state,
+        value: state.value * action.payload
+      }
     default:
       return state;
   }
 }
 
-let state = reducer(initialState, {type: 'INC'});
-state = reducer(state, {type: 'INC'});
-state = reducer(state, {type: 'INC'});
-state = reducer(state, {type: 'INC'});
-console.log(state);
+const store = createStore(reducer);
+
+const update = () => {
+  document.getElementById('counter').textContent = store.getState().value;
+}
+
+store.subscribe(update);
+
+const inc = () =>({type: "INC"});
+const dec = () =>({type: "DEC"});
+const rnd = (value) =>({type: "RND", payload: value});
+
+document.getElementById('inc').addEventListener('click', () => {
+  store.dispatch(inc());
+})
+document.getElementById('dec').addEventListener('click', () => {
+  store.dispatch(dec());
+})
+document.getElementById('rnd').addEventListener('click', () => {
+  const value = Math.floor(Math.random() * 10);
+  store.dispatch(rnd(value),);
+})
+
+
+// let state = reducer(initialState, {type: 'INC'});
+// state = reducer(state, {type: 'INC'});
+// state = reducer(state, {type: 'INC'});
+// state = reducer(state, {type: 'INC'});
+// console.log(state);
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-      <App/>
+      <>
+      </>
     </StrictMode>,
 )
