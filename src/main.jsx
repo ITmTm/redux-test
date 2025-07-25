455de28e106d50dab4dac542bac78b0023b7fb3c
mtm <Stafr95@mail.ru>
2025-07-25 20:32:59 +0500

    working with redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import App from './App.jsx'
 
+import './index.css'
+
+const initialState = 0;
+
+const reducer = (state, action) => {
+  if (action.type === 'INC') {
+    return  state + 1;
+  }
+  return 0;
+}
+
+let state = reducer(initialState, {type: 'INC'});
+state = reducer(state, {type: 'INC'});
+state = reducer(state, {type: 'INC'});
+state = reducer(state, {type: 'INC'});
+console.log(state);
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+      <App/>
+    </StrictMode>,
 )
