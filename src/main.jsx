5d1efed376762cd77e6aa695f17ba40bb3c42714
mtm <Stafr95@mail.ru>
2025-07-27 22:44:52 +0500

    using Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,20 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
-import {createStore, bindActionCreators} from "redux";
 
+import {createStore} from "redux";
+import {Provider} from 'react-redux'
 import reducer from "./reducer.js";
-import * as actions from "./actions.js"
 
+import App from "./components/App.jsx";
 
 const store = createStore(reducer);
 
-const {dispatch, subscribe, getState} = store;
-
-const update = () => {
-  document.getElementById('counter').textContent = getState().value;
-}
-
-subscribe(update);
-
-// const bindActionCreator = (creator, dispatch) => (...args) => {
-//   dispatch(creator(...args));
-// }
-
-const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
-// const decDispatch = bindActionCreators(dec, dispatch);
-// const rndDispatch = bindActionCreators(rnd, dispatch);
-
-document.getElementById('inc').addEventListener('click', inc)
-document.getElementById('dec').addEventListener('click', dec)
-document.getElementById('rnd').addEventListener('click', () => {
-  const value = Math.floor(Math.random() * 10);
-  rnd(value);
-})
-
-
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-      <>
-      </>
+      <Provider store={store}>
+        <App/>
+      </Provider>
     </StrictMode>,
 )
+
+
